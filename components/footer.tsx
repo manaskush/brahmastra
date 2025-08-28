4fe37dc7607e38dd045319a8ca9f9e931fee35ed
ManasKush <manaskush311@gmail.com>
2025-08-28 16:28:25 +0530

    zim

--- components/footer.tsx
+++ components/footer.tsx
@@ -260,7 +260,7 @@ const FooterComponent: React.FC = () => {
                   </div>
                   <div className="flex items-center text-gray-400">
                     <Mail className="w-5 h-5 text-blue-400 mr-3 flex-shrink-0" />
-                    <span>hello@fitlightning.com</span>
+                    <span>hello@brahmastragym.com</span>
                   </div>
                   <div className="flex items-center text-gray-400">
                     <Clock className="w-5 h-5 text-blue-400 mr-3 flex-shrink-0" />
